8428f5cfb9755e8f3807b17fef08e68754365b33
Jinkim2604 <jinkim2604@gmail.com>
2024-05-31 14:50:44 +0200

    fixed b ug

--- app/src/main/java/de/fra_uas/fb2/mobileApplicationExcercises/kitchapp/MainActivity.kt
+++ app/src/main/java/de/fra_uas/fb2/mobileApplicationExcercises/kitchapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package de.fra_uas.fb2.mobileApplicationExcercises.kitchapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
