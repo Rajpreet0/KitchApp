dea3e87ac0d7509f3cb87da72f92c8090be15388
RonHimmel <ronhimmel01@gmail.com>
2024-06-07 15:43:25 +0200

    refactored onClick functions to match the kotlin style and added them to every screen

--- app/src/main/java/de/fra_uas/fb2/mobileApplicationExcercises/kitchapp/MainActivity_pantry.kt
+++ app/src/main/java/de/fra_uas/fb2/mobileApplicationExcercises/kitchapp/MainActivity_pantry.kt
@@ -1,6 +1,8 @@
 package de.fra_uas.fb2.mobileApplicationExcercises.kitchapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,5 +14,22 @@ class MainActivity_pantry : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main_pantry)
 
+    }
+    fun homeButton(view: View){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+    fun groceryButton(view: View){
+        val intent = Intent(this, MainActivity_Groceery::class.java)
+        startActivity(intent)
+    }
+    fun recipesButton(view: View){
+        val intent = Intent(this, MainActivity_recipes::class.java)
+        startActivity(intent)
+    }
+
+    fun profileButton(view: View){
+        val intent = Intent(this, MainActivity::class.java)                            //add profile screen here if its added
+        startActivity(intent)
     }
 }
