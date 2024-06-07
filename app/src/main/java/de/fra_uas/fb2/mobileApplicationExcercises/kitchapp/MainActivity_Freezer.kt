bddd944c17ac54a4fe08f3c57f95f02bf1edc168
RonHimmel <ronhimmel01@gmail.com>
2024-06-07 13:55:53 +0200

    test button

--- app/src/main/java/de/fra_uas/fb2/mobileApplicationExcercises/kitchapp/MainActivity_Freezer.kt
+++ app/src/main/java/de/fra_uas/fb2/mobileApplicationExcercises/kitchapp/MainActivity_Freezer.kt
@@ -1,6 +1,7 @@
 package de.fra_uas.fb2.mobileApplicationExcercises.kitchapp
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,5 +13,10 @@ class MainActivity_Freezer : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main_freezer)
 
+    }
+
+    fun editFreezer(view: View) {
+        //val intent = Intent(this, EditFreezer::class.java)
+        //startActivity(intent)
     }
 }
