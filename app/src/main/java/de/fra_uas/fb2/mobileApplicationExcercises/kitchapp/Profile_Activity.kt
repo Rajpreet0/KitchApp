5609b285d7b2798d25783ac1d895e804d8109f7a
dxriax <153857433+dxriax@users.noreply.github.com>
2024-06-11 20:46:33 +0200

    created profile screen - complete

---
+++ app/src/main/java/de/fra_uas/fb2/mobileApplicationExcercises/kitchapp/Profile_Activity.kt
@@ -0,0 +1,29 @@
+package de.fra_uas.fb2.mobileApplicationExcercises.kitchapp
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+
+class Profile_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_profile)
+        val spinner: Spinner = findViewById(R.id.spLanguage)
+        // Create an ArrayAdapter using the string array and a default spinner layout.
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.languages,
+            R.layout.spinner_items
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears.
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner.
+            spinner.adapter = adapter
+        }
+    }
+}
