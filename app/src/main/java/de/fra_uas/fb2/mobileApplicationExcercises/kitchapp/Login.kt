097da6dd3b1f97376959df51ee3dc40bf6d9d810
RonHimmel <ronhimmel01@gmail.com>
2024-06-13 16:58:14 +0200

    implemented login as starting activity and created button functions

--- app/src/main/java/de/fra_uas/fb2/mobileApplicationExcercises/kitchapp/Login.kt
+++ app/src/main/java/de/fra_uas/fb2/mobileApplicationExcercises/kitchapp/Login.kt
@@ -1,6 +1,8 @@
 package de.fra_uas.fb2.mobileApplicationExcercises.kitchapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,17 @@ class Login : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    fun signUpButton(view: View){
+        val intent = Intent(this, createaccount::class.java)
+        startActivity(intent)
+    }
+    fun loginButton(view: View){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+    fun forgotPasswordButton(view: View){
+        //to be designed
     }
 }
