fb2e4e2c502827d9b4b8dc7261923e0859719297
RonHimmel <ronhimmel01@gmail.com>
2024-06-13 18:59:04 +0200

    recipe creation screens are now connected with next buttons

--- app/src/main/java/de/fra_uas/fb2/mobileApplicationExcercises/kitchapp/recipepreferences.kt
+++ app/src/main/java/de/fra_uas/fb2/mobileApplicationExcercises/kitchapp/recipepreferences.kt
@@ -1,6 +1,8 @@
 package de.fra_uas.fb2.mobileApplicationExcercises.kitchapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,26 @@ class recipepreferences : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+    fun homeButton(view: View){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+    fun groceryButton(view: View){
+        val intent = Intent(this, MainActivityGrocery::class.java)
+        startActivity(intent)
+    }
+    fun recipesButton(view: View){
+        val intent = Intent(this, MainActivityRecipes::class.java)
+        startActivity(intent)
+    }
+
+    fun profileButton(view: View){
+        val intent = Intent(this, ProfileActivity::class.java)
+        startActivity(intent)
+    }
+    fun nextButton(view: View) {
+        val intent = Intent(this, Suggestions::class.java)
+        startActivity(intent)
     }
 }
