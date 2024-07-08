df7c1e712cad298d85d27f3fd2a74fe6cf88e036
dxriax <153857433+dxriax@users.noreply.github.com>
2024-07-09 01:18:06 +0200

    revert

--- app/src/main/java/de/fra_uas/fb2/mobileApplicationExcercises/kitchapp/ActivityProfile.kt
+++ app/src/main/java/de/fra_uas/fb2/mobileApplicationExcercises/kitchapp/ActivityProfile.kt
@@ -1,37 +1,17 @@
 package de.fra_uas.fb2.mobileApplicationExcercises.kitchapp
 
 import android.content.Intent
-import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
 import android.widget.ArrayAdapter
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.LinearLayout
 import android.widget.Spinner
-import android.widget.TextView
-import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
+
 class ActivityProfile : AppCompatActivity() {
-
-    private lateinit var etName: EditText
-    private lateinit var etEmail: EditText
-    private lateinit var icSave: ImageView
-    private lateinit var containerIngr: LinearLayout
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
-
-        // Initialize views
-        etName = findViewById(R.id.etName)
-        etEmail = findViewById(R.id.etEmail)
-        icSave = findViewById(R.id.iconSave)
-        containerIngr = findViewById(R.id.containerIngredients)
 
         setupSpinner(R.id.spLanguage, R.array.languages, R.layout.spinner_items_profile)
     }
@@ -82,107 +62,5 @@ class ActivityProfile : AppCompatActivity() {
         startActivity(intent)
     }
 
-    // Tracks whether the profile is being edited or not
-    private var isEditing = false
-
-    fun profileEditButton(view: View) {
-        isEditing = !isEditing
-        etName.isEnabled = isEditing
-        etName.isFocusable = isEditing
-        etName.isFocusableInTouchMode
-        etEmail.isFocusableInTouchMode
-        etEmail.isEnabled = isEditing
-        etEmail.isFocusable = isEditing
-
-        // TODO: Enable editing of profile details
-
-        icSave.visibility = if (isEditing) View.VISIBLE else View.GONE
-        updateRemoveButtonsVisibility()
-    }
-
-
-    fun profileSaveButton(view: View) {
-        if (isEditing) {
-            isEditing = false
-            etName.isEnabled = false
-            etName.isFocusable = false
-            etEmail.isEnabled = false
-            etEmail.isFocusable = false
-            icSave.visibility = View.GONE
-            updateRemoveButtonsVisibility()
-
-            // TODO: Save profile changes to database
-
-            Toast.makeText(this, "Profile updated", Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    fun addExclButton(view: View) {
-        try {
-            val dialogView = layoutInflater.inflate(R.layout.popup_profile, null)
-            val ingredientText = dialogView.findViewById<EditText>(R.id.editText_ingredient)
-
-            AlertDialog.Builder(this).apply {
-                setTitle("Add ingredient to exclude")
-                setView(dialogView)
-                setPositiveButton("Add") { dialog, _ ->
-                    val inputIngredient = ingredientText?.text?.toString()?.trim() ?: ""
-
-                    if (inputIngredient.isEmpty()) {
-                        Toast.makeText(
-                            this@ActivityProfile,
-                            "Please fill the field",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    } else {
-                        addIngredientToView(inputIngredient)
-                        // TODO: Add ingredient to exclude to database
-                    }
-                }
-                setNegativeButton("Cancel") { dialog, _ ->
-                    dialog.dismiss()
-                }
-                create().show()
-            }
-        } catch (e: Exception) {
-            Log.e("AddExclButton", "Error in addExclButton: ${e.message}")
-            Toast.makeText(this, "An error occurred. Please try again.", Toast.LENGTH_SHORT).show()
-        }
-    }
-
-
-    private fun updateRemoveButtonsVisibility() {
-        Log.d("UpdateVisibility", "updateRemoveButtonsVisibility called. isEditing: $isEditing")
-        for (i in 0 until containerIngr.childCount) {
-            val childView = containerIngr.getChildAt(i)
-            val icRemove = childView.findViewById<ImageView>(R.id.icRemove)
-            icRemove.visibility = if (isEditing) View.VISIBLE else View.GONE
-            Log.d("UpdateVisibility", "Ingredient $i visibility: ${icRemove.visibility}")
-        }
-    }
-
-
-    private fun addIngredientToView(ingredient: String) {
-        try {
-            val inflater = LayoutInflater.from(this)
-            val rowView = inflater.inflate(R.layout.profile_ingredient_layout, containerIngr, false)
-
-            val tvIngredient = rowView.findViewById<TextView>(R.id.tvIngredientName)
-            tvIngredient.text = ingredient
-
-            val icRemove = rowView.findViewById<ImageView>(R.id.icRemove)
-
-            icRemove.setOnClickListener {
-                containerIngr.removeView(rowView)
-                // TODO: Remove ingredient from database
-            }
-
-            containerIngr.addView(rowView)
-
-            Toast.makeText(this, "Ingredient added: $ingredient", Toast.LENGTH_SHORT).show()
-        } catch (e: Exception) {
-            Log.e("AddIngredient", "Error adding ingredient: ${e.message}")
-            Toast.makeText(this, "Error adding ingredient. Please try again.", Toast.LENGTH_SHORT).show()
-        }
-    }
 }
+
