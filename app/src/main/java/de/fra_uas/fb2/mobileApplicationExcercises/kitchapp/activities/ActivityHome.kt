816da9622390e4d7f5c29fb78bbe31cba6b500e9
Rajpreet0 <Rajpreet.Singh@gmx.de>
2024-07-11 20:20:05 +0200

    added suggestions in home

--- app/src/main/java/de/fra_uas/fb2/mobileApplicationExcercises/kitchapp/activities/ActivityHome.kt
+++ app/src/main/java/de/fra_uas/fb2/mobileApplicationExcercises/kitchapp/activities/ActivityHome.kt
@@ -4,25 +4,40 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.ProgressBar
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import de.fra_uas.fb2.mobileApplicationExcercises.kitchapp.ActivityProfile
 import de.fra_uas.fb2.mobileApplicationExcercises.kitchapp.R
+import de.fra_uas.fb2.mobileApplicationExcercises.kitchapp.fragments.LoadingDialogFragment
 import de.fra_uas.fb2.mobileApplicationExcercises.kitchapp.helpers.NetworkHelper
 import de.fra_uas.fb2.mobileApplicationExcercises.kitchapp.helpers.SessionManager
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import org.json.JSONArray
+import org.json.JSONObject
 import java.io.IOException
 
 class ActivityHome : AppCompatActivity() {
 
     private lateinit var sessionManager: SessionManager
     private lateinit var tvGreeting: TextView
+    private lateinit var btnSuggestions1: AppCompatButton
+    private lateinit var btnSuggestions2: AppCompatButton
+    private lateinit var progessCircular: ProgressBar
+
+    private var response: String = ""
+    private val portion: String = "1"
+
+    private val networkHelper = NetworkHelper()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,10 +51,100 @@ class ActivityHome : AppCompatActivity() {
 
         sessionManager = SessionManager(this)
         tvGreeting = findViewById(R.id.tvGreeting);
+        btnSuggestions1 = findViewById(R.id.btnSuggestions1);
+        btnSuggestions2 = findViewById(R.id.btnSuggestions2)
+        progessCircular = findViewById(R.id.progessCircular);
+
+        btnSuggestions1.visibility = View.GONE
+        btnSuggestions2.visibility = View.GONE
 
         tvGreeting.text = "Hi, ${sessionManager.getUsername()}"
 
+        generateSuggestions()
+
     }
+
+    private fun generateSuggestions() {
+         CoroutineScope(Dispatchers.IO).launch {
+             try {
+                val response = withContext(Dispatchers.IO) {
+                    networkHelper.suggestRecipe(
+                        portion,
+                        "",
+                        "",
+                        "",
+                        "",
+                        "",
+                        "",
+                        "",
+                        true
+                    )
+                }
+
+                 withContext(Dispatchers.Main) {
+                    this@ActivityHome.response = response.toString()
+                     try {
+                         val jsonResponse = JSONObject(response.toString())
+
+                         if (jsonResponse.has("recipes") && jsonResponse.get("recipes") is JSONArray) {
+                             val recipesArray = jsonResponse.getJSONArray("recipes")
+
+                             val firstRecipe = recipesArray.getJSONObject(0).getJSONArray("recipes").getJSONObject(0)
+                             val firstRecipeTitle = firstRecipe.getString("name")
+
+                             btnSuggestions1.setText(firstRecipeTitle)
+                             btnSuggestions1.visibility = View.VISIBLE
+                             progessCircular.visibility = View.GONE
+
+                             if (recipesArray.length() >= 2) {
+                                 val secondRecipe = recipesArray.getJSONObject(1).getJSONArray("recipes").getJSONObject(0)
+                                 val secondRecipeTitle = secondRecipe.getString("name")
+                                 btnSuggestions2.setText(secondRecipeTitle)
+                                 btnSuggestions2.visibility = View.VISIBLE
+                             }
+
+                         } else {
+                             Log.d("Recipes: ", "Recipes key is not a JSONArray or does not exist")
+                             progessCircular.visibility = View.GONE
+                             Toast.makeText(applicationContext, "Error loading suggestions", Toast.LENGTH_SHORT).show()
+                         }
+                     } catch (e: Exception) {
+                         Log.e("JSON Parsing Error", "Error parsing JSON response", e)
+                         progessCircular.visibility = View.GONE
+                         Toast.makeText(applicationContext, "Loading Suggenstions Failed", Toast.LENGTH_SHORT).show()
+                     }
+                 }
+             } catch (e: IOException) {
+                 Log.d("SERVER ERROR", "${e}")
+                 withContext(Dispatchers.Main) {
+                     Toast.makeText(
+                         applicationContext,
+                         "SERVER ERROR: Prompt Error",
+                         Toast.LENGTH_SHORT
+                     ).show()
+                 }
+             }
+         }
+    }
+
+    private fun changeTwoSugesstions(name: String) {
+        val intent: Intent = Intent(this, ActivityRecipeDisplay::class.java).apply {
+            putExtra("response", response)
+            putExtra("name", name)
+            putExtra("portion", portion)
+        }
+        startActivity(intent)
+        finish()
+    }
+
+    fun suggestionBtnOne(view: View) {
+        changeTwoSugesstions(btnSuggestions1.text.toString())
+    }
+
+    fun suggestionBtnTwo(view: View) {
+        changeTwoSugesstions(btnSuggestions2.text.toString())
+    }
+
     fun createRecipeButton(view: View){
         val intent = Intent(this, ActivityExcludeIngredients::class.java)
         startActivity(intent)
